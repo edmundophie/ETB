5a3d891d5d30b5b75faddcd93ca5ce88ed1c531a
adwisatya <a.dwisaty4@yahoo.com>
2014-11-13 15:27:16 +0700

    Katalog & Search

---
+++ js/novel-slideshow.js
@@ -0,0 +1,23 @@
+    var novel_ganti= 1;
+    var novel_jumlahgambar= 3;
+    var novel_kecepatanganti=2000;
+
+    var novel_img1=new Image();
+    var novel_img2=new Image();
+    var novel_img3=new Image();
+
+    novel_img1.src="img/katalog/olahraga/1.jpg";
+    novel_img2.src="img/katalog/olahraga/2.jpg";
+    novel_img3.src="img/katalog/olahraga/3.jpg";
+
+    function novel_ganti_gambar(){
+		if (document.images)
+		{
+			document.images.gambarnovel.src=eval("novel_img"+ganti+".src");
+			if (novel_ganti<novel_jumlahgambar)
+			novel_ganti++;
+			else
+			novel_ganti= 1;
+		}
+		setTimeout("novel_ganti_gambar()",novel_kecepatanganti);
+    }
---
+++ js/science-slideshow.js
@@ -0,0 +1,23 @@
+    var science_ganti= 1;
+    var science_jumlahgambar= 3;
+    var science_kecepatanganti=2000;
+
+    var science_img1=new Image();
+    var science_img2=new Image();
+    var science_img3=new Image();
+
+    science_img1.src="img/katalog/olahraga/1.jpg";
+    science_img2.src="img/katalog/olahraga/2.jpg";
+    science_img3.src="img/katalog/olahraga/3.jpg";
+
+    function science_ganti_gambar(){
+		if (document.images)
+		{
+			document.images.gambarscience.src=eval("science_img"+ganti+".src");
+			if (science_ganti<science_jumlahgambar)
+			science_ganti++;
+			else
+			science_ganti= 1;
+		}
+		setTimeout("science_ganti_gambar()",science_kecepatanganti);
+    }
