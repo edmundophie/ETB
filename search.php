340a10bec135839d74e64acba68fe8dcbc603a82
adwisatya <a.dwisaty4@yahoo.com>
2014-11-16 21:43:12 +0700

    Autocomplete search

--- search.php
+++ search.php
@@ -1,49 +1,8 @@
-<html>
-	<head>
-		<title>
-		</title>
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-	</head>
-	<body onload="">
-		<center>
-			<div id="container" style="width:960px">
-				<table class="table table-condensed">
-					<thead>
-						<tr>
-							<th>Row</th>
-							<th>First Name</th>
-							<th>Last Name</th>
-							<th>Email</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr onClick="location.href = 'http://www.itb.ac.id'">
-							<td>1</td>
-							<td>John</td>
-							<td>Carter</td>
-							<td>johncarter@mail.com</td>
-						</tr>
-						<tr>
-							<td>2</td>
-							<td>Peter</td>
-							<td>Parker</td>
-							<td>peterparker@mail.com</td>
-						</tr>
-						<tr>
-							<td>3</td>
-							<td>John</td>
-							<td>Rambo</td>
-							<td>johnrambo@mail.com</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
-		</center>
-	<script src="js/olahraga-slideshow.js"></script>
-	<script src="js/seni-slideshow.js"></script>
-	<script src="js/teknik-slideshow.js"></script>
-	<script src="js/science-slideshow.js"></script>
-	<script src="js/novel-slideshow.js"></script>
-
-	</body>
-</html>
+<?php
+$q = trim(strip_tags($_GET['term'])); // variabel $q untuk mengambil inputan user
+$daftar = file("data.txt");
+foreach ($daftar as $data){
+	$result[] = htmlentities(stripslashes($data));
+}
+echo json_encode($result); // menampilkan data dengan format json
+?>
